047c00a4bc1c759ab6f32bf37c64abf4ace48540
Jatin Sharma <jatinsharma21092002@gmail.com>
2022-06-23 12:20:00 +0530

    first api project

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests")
+const homeFile = fs.readFileSync("home.html","utf-8");
+
+const replaceVal = (tempVal,orgVal)=>{
+
+    let temperature = tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}",orgVal.name);
+    temperature = temperature.replace("{%country%}",orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}",orgVal.weather[0].main);
+    return temperature;
+}
+const server = http.createServer((req,res)=>{
+    if(req.url == "/"){
+        requests("https://api.openweathermap.org/data/2.5/weather?q=bareilly&appid=5dc89b30e04e9afc1a65059f9e033199")
+        .on("data",(chunk)=>{
+            const objdata = JSON.parse(chunk);
+            const arrData = [objdata];
+            const realTimeData = arrData.map((val)=>replaceVal(homeFile,val)).join("");
+            res.write(realTimeData);
+            // console.log(realTimeData);
+        })
+        .on("end",(err)=>{
+            if(err) return console.log("connection closed due to error")
+             res.end;
+        })
+    }
+})
+
+server.listen(3000,"127.0.0.1");
